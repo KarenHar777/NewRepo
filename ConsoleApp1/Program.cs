d17381a2cec1162ec7e3fcfc15ca7339767dd802
KarenHarutyunyan <karenvardanovich@gmail.com>
2022-11-22 12:07:11 +0400

    Add bool default

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,7 +10,10 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("New Year");
+            bool chack = false;
+            Console.WriteLine("bool default: " + chack);
+
+            Console.ReadKey();
         }
     }
 }
